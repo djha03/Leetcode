af776021abb23b072c37614a341c6c1da2ba6d00
Divakar kumar Jha <divakarjha9128@gmail.com>
2024-06-07 20:01:48 +0530

    Time: 54 ms (25.21%), Space: 12.7 MB (74.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,6 +5,7 @@ public:
             for(int j=i+1; j<nums.size(); j++){
                 if(nums[i]+nums[j]==target){
                     return {i,j};
+
                 }
             }
         }
