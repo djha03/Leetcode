8883abcc25455a53a52a2e41ecc356b9151868aa
Divakar kumar Jha <divakarjha9128@gmail.com>
2024-10-05 22:13:49 +0530

    Time: 57 ms (34.91%), Space: 49.3 MB (18.29%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+        let i;
+        let s=0;
+        let n=nums.length;
+
+        if(n==0)
+        return 0;
+        for(i=0; i<n; i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[s++]=nums[i];
+            }
+        }
+        return s;
+    }
