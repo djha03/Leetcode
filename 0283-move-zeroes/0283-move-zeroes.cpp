aa84dbebb4390efbad419f08e4dc2f3801b00b5c
Divakar kumar Jha <divakarjha9128@gmail.com>
2024-10-16 00:39:05 +0530

    Time: 16 ms (53.18%), Space: 21.7 MB (89.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-         vector < int > nonZero;
-    vector < int > zero;
+        // //!BRUTE-FORRCE APPROACH
+        // vector<int>temp; //created a new vector
+        // int n=nums.size();
+        // for(int i=0; i<n; i++) {
+        //     if(nums[i]!=0) {
+        //         temp.push_back(nums[i]);
+        //     }
+        // }
 
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] != 0) {
-            nonZero.push_back(nums[i]);
-        } else {
-            zero.push_back(nums[i]);
-        }
-    }
-    nums.clear();
+        // int nz=temp.size();
+        // for(int i=0; i<nz; i++) {
+        //     nums[i]=temp[i];
+        // }
+        
+        // for(int i=nz; i<n; i++) {
+        //     nums[i]=0;
+        // }
 
-    for (int i = 0; i < nonZero.size(); i++) {
-        nums.push_back(nonZero[i]);
-    }
-    for (int i = 0; i < zero.size(); i++) {
-        nums.push_back(zero[i]);
-    }
+        //OPTIMAL SOLN
+       int i=0;
+       for(int j=0; j<nums.size(); j++) {
+           if(nums[j]!=0) {
+               swap(nums[j], nums[i]);
+               i++;
+           }
+       }
     }
 };
